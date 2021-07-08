2df14e45ffe65c34b87c2f32423f3091991cb14d
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2021-07-08 22:18:54 +0530

    Add files via upload

---
+++ book.get.js
@@ -0,0 +1,28 @@
+const Book = require("../controller/book.get.js");
+
+// Retrieve all Customers from the database.
+exports.findAll = (req, res) => {
+    Book.getAll((err, data) => {
+      if (err)
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving customers."
+        });
+      else res.send(data);
+    });
+  };
+
+
+  // Retrieve the pagination of the book 
+
+  exports.findAll = (req, res) => {
+    Book.getAll((err, data) => {
+      if (err)
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving customers."
+        });
+      else res.send(data);
+    });
+  };
+
