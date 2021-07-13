f05e1e746f72841810d3043cd67753cc13dbabae
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2021-07-13 19:55:33 +0530

    Add files via upload

--- controllers/login.js
+++ controllers/login.js
@@ -1,31 +1,46 @@
 const userModel = require.main.require('./models/userModel');
-// const jwt= require('jsonwebtoken');
+const jwt= require('jsonwebtoken');
 
 
-exports.loginuser = function(req, res){
+exports.login = function(req, res){
     var data = {
   
   email: req.body.email,
   password: req.body.password,
   
 };
-var token = jwt.sign({email:user.email}, 
-    "mynameiaankurbackenddeveloper", {
-        expiresIn:"7days"
-    })
-    res.send(token)
+ userModel.loginUser(req.body.email, function(result){
+     if(!result){
+       res.render('login',{errs:[{message:'invalid email'}]});
+       var token = jwt.sign({email:user.email}, 
+        "mynameiaankurbackenddeveloper", {
+            expiresIn:"7days"
+        })
 
- userModel.loginUser(data, function(result){
-     if(result){
+        res.send({"status":"200","statuscode":"1","result":result});
+        console.log(token);
         console.log(result);
-        res(result);
+    
+        
     }
     else {
-        res('Invalid');
+      
+      res('valid   email ');
     }
     });
   console.log(req.body);
 }
 
 
+
+
+
  
+
+
+
+  //   app.get("/userData/",function(req,res){
+  //     ogetData._getData(function(data){
+  //         res.send({"status":"200","statuscode":"1","result":data});
+  //     });
+  // })
