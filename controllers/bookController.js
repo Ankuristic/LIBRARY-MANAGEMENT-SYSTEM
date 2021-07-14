72fd1c65903c014f17174182130c4d2a85fd1be3
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2021-07-14 18:11:24 +0530

    Add files via upload

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -54,16 +54,46 @@ exports.bookGetByStudent=(req,res)=>{
 
 // get all borrowed book 
 exports.getAllBorrowed=(req,res)=>{
-  bookModel.storedAllBorrowedBooks((err,data)=>{
-    console.log(err);
-    console.log(data);
+  const  data={
+    book_id: req.body.book_id,
+    user_id:req.body.user_id,
+    issue_date:req.body.issue_date
 
-      if (err)
-    res.status(500).send({
-      message:
-        err.message || "Some error occurred ."
-    });
-  else res.send(data);
-  })
-  
+  }
+
+  bookModel.storedAllBorrowedBooks(req.body.user_id, function(err,data){
+    if(data)
+      res.send({
+      const token = jwt.sign({user:user_id}, 
+       "mynameiaankurbackenddeveloper", 
+       {
+           expiresIn:"7days"
+       })
+      })
+       console.log(token);
+       // console.log(result);
+   
+   else {
+     res(err);
+  }
+
+   });
 }
+
+
+//   bookModel.storedAllBorrowedBooks((err,data)=>{
+//     console.log(err);
+//     console.log(data);
+
+//       if (err)
+//     res.status(500).send({
+//       message:
+//         err.message || "Some error occurred ."
+//     });
+//   else res.send(data);
+//   })
+  
+// }
+
+
+
